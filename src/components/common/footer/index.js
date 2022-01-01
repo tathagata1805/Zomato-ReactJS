85c4a680e3a42c5f80eec4833841a545efe59d73
Ahana Sen <ahana.sen.uemk.cs.2023@gmail.com>
2022-01-01 20:35:44 +0530

    first commit

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="absolute-center max-width">Made by Tathagata Chakraborty || 2022</div>
+  );
+};
+
+export default Footer;
